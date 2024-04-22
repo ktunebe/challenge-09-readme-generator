16af362cc070ca576596dc180175a521c270722f
ktunebe <ktunebe@gmail.com>
2024-04-22 18:35:38 -0500

    Put in rough template for README, outlined questions

---
+++ Develop/README.md
@@ -0,0 +1,41 @@
+# fwe
+
+## Description
+
+fewqw
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+wefew
+
+## Usage
+
+fewfwe
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+## Contributing
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+## Questions
+
+Any questions about this application should be directed to:
+
+My GitHub: https://github.com/fese
+My Email: fsefe
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,124 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: 'What is the title of your project?',
+        name: 'titleInput'
+    },
+    {
+        message: 'How would you describe this project?',
+        name: 'descriptionInput'
+    },
+    {
+        message: 'How do you install this application?',
+        name: 'installationInput'
+    },
+    {
+        message: 'How do you use this application?',
+        name: 'usageInput'
+    },
+    {
+        type: 'list',
+        message: 'What type of license does this application have?',
+        choices: ['1', '2', '3'],
+        name: 'licenseInput'
+    },
+    {
+        message: 'How do you contribute to this application?',
+        name: 'contributingInput'
+    },
+    {
+        message: 'How can you test this application?',
+        name: 'testInput'
+    },
+    {
+        message: 'What is your GitHub username?',
+        name: 'githubInput'
+    },
+    {
+        message: 'What is your email address?',
+        name: 'emailInput'
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToREADME = (responseText) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('README.md', responseText, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve('Data written to README.md');
+            }
+        });
+    });
+};
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            const {titleInput, descriptionInput, installationInput, usageInput, licenseInput, contributingInput, testInput, githubInput, emailInput} = answers;
+
+            // const responseText = `Title: ${titleInput}\nDescription: ${descriptionInput}\nInstallation: ${installationInput}\nUsage: ${usageInput}\nLicense: ${licenseInput}\nContributing: ${contributingInput}\nTesting: ${testInput}\nGitHub: ${githubInput}\nEmail: ${emailInput}`
+
+            responseText = 
+`# ${titleInput}
+
+## Description
+
+${descriptionInput}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${installationInput}
+
+## Usage
+
+${usageInput}
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+## Contributing
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+## Questions
+
+Any questions about this application should be directed to:
+
+My GitHub: https://github.com/${githubInput}
+My Email: ${emailInput}`
+
+            writeToREADME(responseText)
+        })
+        .catch(error => {
+            console.error('Error occurred:', error);
+        });
+}
 
 // Function call to initialize app
 init();
+
